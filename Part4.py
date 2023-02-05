e5082ea812ef3ac70c4c863776ac173bc310454a
Xingyu <xingyu3906@gmail.com>
2023-02-05 20:55:50 +0800

    Part 4.6

--- Part4.py
+++ Part4.py
@@ -483,7 +483,16 @@ def formatted(my_list : list):
         new.append(newItem)
     return(new)
 
-
+# 4.6.1 Everything reversed
+# Please write a function named `everything_reversed`, which takes a list of 
+# strings as its argument. The function returns a new list with all of the items
+# on the original list reversed. Also the order of items should be reversed on 
+# the new list.
+def everything_reversed(my_list : list):
+    new_list = []
+    for item in my_list[::-1]:
+        new_list.append(item[::-1])
+    return(new_list)
 
 
 
