a33db2c1dc45a7cfe68e40306718f1b58a344ad7
Xingyu <xingyu3906@gmail.com>
2023-02-10 17:26:27 +0800

    Part 5.1

---
+++ Part5.py
@@ -0,0 +1,103 @@
+# 5.1.1 The longest string
+def longest(strings : list):
+    length = 0
+    word = ""
+    for item in strings:
+        if len(item) > length:
+            length = len(item)
+            word = item
+    return word
+
+# 5.1.2 Number of matching elements
+def count_matching_elements(my_matrix: list, element: int):
+    count = 0
+    for row in my_matrix:
+        count += row.count(element) 
+    return count
+
+# 5.1.3 Go
+def who_won(game_board: list):
+    count1 = 0
+    count2 = 0
+    for row in game_board:
+        count1 += row.count(1)
+        count2 += row.count(2)
+    
+    if count1 > count2:
+        return 1
+    elif count1 < count2:
+        return 2
+    else:
+        return 0
+
+# 5.1.4 Sudoku: check row
+def row_correct(sudoku: list, row_no:int):
+    for i in range(len(sudoku[row_no])):
+        number = sudoku[row_no][i]
+        if number == 0:
+            continue
+
+        
+        if sudoku[row_no].count(number) == 1:
+            continue
+        else:
+            return False
+    
+    return True
+
+# 5.1.5 Sudoku: check column
+def column_correct(sudoku: list, column_no: int):
+    temp = []
+    for row in sudoku:
+        number = row[column_no]
+        if number == 0:
+            continue
+
+        if number in temp:
+            return False
+        else:
+            temp.append(row[column_no])
+            
+    return True
+        
+# 5.1.6 Sudoku: check block
+def block_correct(sudoku: list, row_no: int, column_no: int):
+    temp = []
+    for i in range(row_no, row_no+3):
+        for j in range(column_no, column_no+3):
+            number = sudoku[i][j]
+            if number == 0:
+                continue
+
+            if number in temp:
+                return False
+            else:
+                temp.append(number)
+
+    return True
+
+# 5.1.7 Sudoku: check grid
+def sudoku_grid_correct(sudoku: list):
+    # Check rows
+    for i in range(9):
+        if row_correct(sudoku, i):
+            continue
+        else:
+            return False
+
+    # Check columns
+    for i in range(9):
+        if column_correct(sudoku, i):
+            continue
+        else:
+            return False
+
+    # Check block
+    for i in range(0, 7, 3):
+        for j in range(0, 7, 3):
+            if block_correct(sudoku, i, j):
+                continue
+            else:
+                return False
+
+    return True
